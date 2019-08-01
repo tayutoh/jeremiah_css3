879931c10dacbb332b37115cbed17da3b7c18081
tayutoh <tayutoh@gmail.com>
2019-08-01 23:26:19 +0900

    feat: 全体ベース作成 fix: gulp-sassの設定修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-//var plumber = require('gulp-plumber');
+var plumber = require('gulp-plumber');
 
 // sassとcssの保存先
 gulp.task('sass', function(){
-    gulp.src('./sass/**/*.scss')
+    gulp.src('./assets/sass/*.scss')
+        .pipe(plumber())
         .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(gulp.dest('./css/'));
+        .pipe(gulp.dest('./assets/css/'));
 });
 
 //自動監視
 gulp.task('sass-watch', ['sass'], function(){
-    var watcher = gulp.watch('./sass/**/*.scss', ['sass']);
+    var watcher = gulp.watch('./assets/sass/*.scss', ['sass']);
     watcher.on('change', function(event) {
     });
 });
